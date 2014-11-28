8614bcaaee2baf1e401ca2c702890080c1a062a2
Gabriel Horner <gabriel.horner@gmail.com>
2014-11-28 16:18:47 -0500

    Add submit-plugin.rb for speedier plugin submissions

--- README.md
+++ README.md
@@ -12,6 +12,14 @@ new version number e.g. 0.1.2. You can open an issue with the github url of your
 request with the given file. Don't forget that your plugin's repository must be tagged with `VERSION`
 in order to be installed.
 
+For Committers
+==============
+
+There is a `submit-plugin.rb` script to create the plugin file given a github\_url and version
+e.g. `./submit-plugin.rb https://github.com/LightTable/Emmet 0.0.2`.
+This script does _not_ verify the submission is authorized i.e. not clobbering an existing plugin -
+you still need to do that manually. To use the script, make sure you are using ruby >= 2.0 and install the edn gem: `gem install edn`.
+
 License
 =======
 
---
+++ submit-plugin.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+require 'edn'
+require 'json'
+
+def process_args(argv)
+  unless argv.size == 2
+    $stderr.puts "#{$0} GITHUB_URL VERSION"
+    exit 1
+  end
+
+  unless argv[1] =~ /^\d\.\d\.\d$/
+    $stderr.puts "Invalid version. Version must be in the format X.X.X e.g. 0.1.2."
+    exit 1
+  end
+  argv
+end
+
+def get_plugin(github_url, version)
+  base_url = "#{github_url.sub('github.com', 'raw.githubusercontent.com')}/#{version}"
+  ["plugin.edn", open("#{base_url}/plugin.edn").read]
+rescue OpenURI::HTTPError
+  begin
+    ["plugin.json", open("#{base_url}/plugin.json").read]
+  rescue OpenURI::HTTPError
+    $stderr.puts "No plugin.edn or plugin.json exists for that version. Make sure the repository has a tag for that version."
+    exit 1
+  end
+end
+
+def extract_name(filename, plugin_body)
+  if filename == 'plugin.json'
+    JSON.parse(plugin_body)['name']
+  else
+    EDN.read(plugin_body)[:name]
+  end
+end
+
+def add_new_file(plugin_name, version, plugin_body, filename)
+  system("mkdir -p #{plugin_name}/#{version}")
+  File.open("#{plugin_name}/#{version}/#{filename}", 'w') { |f| f.write(plugin_body) }
+end
+
+github_url, version = process_args(ARGV)
+plugin_filename, plugin_body = get_plugin(github_url, version)
+plugin_name = extract_name(plugin_filename, plugin_body)
+add_new_file(plugin_name, version, plugin_body, plugin_filename)
